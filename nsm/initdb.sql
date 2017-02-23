3e00fc85cdae19d1b9c584c498adfd2f4e25e8fd
Anne Rogers <amr@cs.uchicago.edu>
2017-02-23 01:43:35 -0600

    made two tables

--- nsm/initdb.sql
+++ nsm/initdb.sql
@@ -1,10 +1,17 @@
 DROP TABLE players;
+DROP TABLE data;
+
 CREATE TABLE players
-             (id int, name text, team int, g int, ab int, pa int,
+            (id int, ispitcher boolean)
+
+
+CREATE TABLE data
+             (id int, name text, team text, g int, ab int, pa int,
              h int,singles int, doubles int, triples int, homerun int, 
              runs int, runs_batted_in int,
              bb int, ibb int, so int, hbp int, sf int, sh int, gdp int,
-             sb int, cs int, avg int,shift boolean,pitcher boolean);
+             sb int, cs int, avg int, shift boolean, noshift boolean, 
+             trad_shift boolean, nontrad_shift boolean);
 
 
 
