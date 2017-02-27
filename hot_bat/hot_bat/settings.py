8bfff4f4e5351d351523366d6ea5382af4f98333
Federico Scivittaro <fgscivittaro@cs.uchicago.edu>
2017-02-26 18:48:56 -0600

    Created models

--- hot_bat/hot_bat/settings.py
+++ hot_bat/hot_bat/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'hot_bat.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '2016_sqlcode.sqlite3'),
     }
 }
 
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
