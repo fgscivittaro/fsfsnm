ae8b60b04f622d43a0584cc02df1062da81a5a93
Federico Scivittaro <fgscivittaro@cs.uchicago.edu>
2017-03-07 14:53:30 -0600

    Connected db to website

--- hot_bat/query/views.py
+++ hot_bat/query/views.py
@@ -8,7 +8,8 @@ def index(request):
 	The main page view.
 	'''
 
-	top50results = RegularData.objects.order_by('pa')[:50]
-	context = {'top50results': top50results}
+	results = RegularData.objects.filter(shift=0).filter(noshift=0).filter(trad_shift=0).filter(shift=0).order_by('pa')[4000:4050]
+	# Need to figure out how to sort descending
+	context = {'results': results}
 
 	return render(request, 'query/index.html', context)
